f6e1fc7784690b9fa4f914572495817984062c5e
Vladislav Permyakov <vladislav_permyakov@epam.com>
2020-07-04 12:30:20 +0600

    Update sampleFunction

--- src/main/java/Sample/SampleFeature.java
+++ src/main/java/Sample/SampleFeature.java
@@ -3,5 +3,6 @@ package Sample;
 public class SampleFeature {
     void sampleFunction(){
         System.out.println("updated sample function");
+        System.out.println("action from master branch");
     }
 }
